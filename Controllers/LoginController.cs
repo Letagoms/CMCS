523bbf11048303ff26b84f8615c9ca5b0cb5c25c
RC_Student_lab <letagoms5@gmail.com>
2025-09-04 19:21:37 +0000

    i created a login and registration page keeping a similar theme. i'm yet to do some final edits to make the login a bit more appealing, but overall its functioning

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Login(/* login model parameters if any */)
+        {
+            // Validate login credentials here
+
+            // Redirect to Home page after successful login
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+
+    }
+}
